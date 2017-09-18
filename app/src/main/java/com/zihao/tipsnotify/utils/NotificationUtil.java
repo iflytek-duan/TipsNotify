a02a18bfb6367f34ce706f9d77d400d6d4e9c982
zihao <jgduan@iflytek.com>
2017-09-18 09:57:16 +0800

    1.新增清除单个通知方法； 2.修改工具类注释。

--- app/src/main/java/com/zihao/tipsnotify/utils/NotificationUtil.java
+++ app/src/main/java/com/zihao/tipsnotify/utils/NotificationUtil.java
@@ -31,6 +31,11 @@ public class NotificationUtil {
         return NotificationHolders.INSTANCE;
     }
 
+    /**
+     * 显示(推送)一个通知
+     *
+     * @param context context
+     */
     public void showNotification(Context context) {
         if (notificationManager == null) {
             notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -49,7 +54,27 @@ public class NotificationUtil {
         notificationManager.notify(0, notification);
     }
 
-    public void clearNotification(Context context) {
+
+    /**
+     * 根据通知id清除指定的通知
+     *
+     * @param context context
+     * @param id      要清除通知的id
+     */
+    public void clearNotification(Context context, int id) {
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        notificationManager.cancel(id);
+    }
+
+    /**
+     * 清除所有通知
+     *
+     * @param context context
+     */
+    public void clearAllNotification(Context context) {
         if (notificationManager == null) {
             notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         }
@@ -57,6 +82,16 @@ public class NotificationUtil {
         notificationManager.cancelAll();
     }
 
+    /**
+     * 构建一个通知对象
+     *
+     * @param context       context
+     * @param contentTitle  通知的标题
+     * @param contentText   通知的内容概述
+     * @param tickerText    通知发出且用户还未下拉通知栏时通知在通知栏上展示的信息
+     * @param pendingIntent pendingIntent 用户定义的填充意向
+     * @return Notification
+     */
     private Notification builderNotification(Context context, CharSequence contentTitle,
                                              CharSequence contentText, CharSequence tickerText,
                                              PendingIntent pendingIntent) {
@@ -74,6 +109,9 @@ public class NotificationUtil {
         notification.flags |= Notification.FLAG_HIGH_PRIORITY;
         // 表明在点击了通知栏中的"清除通知"后，此通知不清除，
         // 经常与FLAG_ONGOING_EVENT一起使用
+//        notification.flags |= Notification.FLAG_NO_CLEAR;
+
+        // 这里因需求变更修改为点击后自动消失
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
 
         return notification;
